b4b1650bc7a0a1137e74ec6521ee5c56bcbd57f4
Marcos <kerallis2015@gmail.com>
2023-05-13 00:36:40 -0300

    feat: instalando e configurando chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,28 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
+import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-import { ThemeProvider } from 'styled-components';
+import { ChakraProvider } from '@chakra-ui/react'
+import { ThemeProvider } from 'styled-components'
 
-import GlobalStyles from 'styles/global';
-import theme from 'styles/theme';
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>App Name</title>
-        <link rel="shorcut icon" href="/img/bg.png" />
-        <link rel="apple-touch-icon" href="/img/bg.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="App Name" />
-      </Head>
-      <Component {...pageProps} />
-      <GlobalStyles />
-    </ThemeProvider>
-  );
+    <ChakraProvider>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>App Name</title>
+          <link rel="shorcut icon" href="/img/bg.png" />
+          <link rel="apple-touch-icon" href="/img/bg.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="description" content="App Name" />
+        </Head>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </ThemeProvider>
+    </ChakraProvider>
+  )
 }
 
-export default App;
+export default App
