fa151ce9ad1982150aaac05b867c5a4b9603c824
Marcos <kerallis2015@gmail.com>
2023-05-27 23:11:43 -0300

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,11 +9,11 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={CHACKRA_UI_THEME}>
       <Head>
-        <title>App Name</title>
+        <title>Pecuária - Painel Web</title>
         <link rel="shorcut icon" href="/img/bg.png" />
         <link rel="apple-touch-icon" href="/img/bg.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="App Name" />
+        <meta name="description" content="Pecuária - Painel Web" />
       </Head>
       <AuthProvider>
         <Component {...pageProps} />
